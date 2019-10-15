571336f3392a6e2ef2f99fd2a2cbb600a42fb87f
rrkheloufi <rayan-ryad.kheloufi@etu.u-bordeaux.fr>
2019-10-15 11:26:16 +0200

    Nouvelle archi

--- baseDeDonnees/inMemoryWorkshop.js
+++ baseDeDonnees/inMemoryWorkshop.js
@@ -1,45 +1,45 @@
+/*global inMemoryWorkshop:true, module:true*/
 inMemoryWorkshop = []
 
-
 function getWorkshopList() {
-    return new Promise((resolve, ) => {
-        resolve(inMemoryWorkshop)
-    })
+  return new Promise((resolve, ) => {
+    resolve(inMemoryWorkshop)
+  })
 }
 
-function getWorkshopByName(name) {
-    return new Promise((resolve, reject) => {
-        if (!name) {
-            reject(new Error("name parameter is required"))
-        }
-        resolve(inMemoryWorkshop.find(workshop => workshop.name === workshop))
-    })
-}
+// function getWorkshopByName(name) {
+//   return new Promise((resolve, reject) => {
+//     if (!name) {
+//       reject(new Error("name parameter is required"))
+//     }
+//     resolve(inMemoryWorkshop.find(workshop => workshop.name === workshop))
+//   })
+// }
 
 function getWorkshopById(id) {
-    return new Promise((resolve, reject) => {
-        if(id > inMemoryWorkshop.length || id < 0){
-            reject(new Error("Not valid id"))
-      	}
-        resolve(inMemoryWorkshop[id])
-    })
+  return new Promise((resolve, reject) => {
+    if(id > inMemoryWorkshop.length || id < 0){
+      reject(new Error("Not valid id"))
+    }
+    resolve(inMemoryWorkshop[id])
+  })
 }
 
 
 function addWorkshop(name, description) {
-    return new Promise((resolve, reject) => {
-        if (!name) {
-            reject(new Error("Workshop name required"))
-        }
-        if (!description) {
-            reject(new Error("Workshop description required"))
-        }
-        inMemoryWorkshop.push({
-            name,
-            description
-        })
-        resolve()
+  return new Promise((resolve, reject) => {
+    if (!name) {
+      reject(new Error("Workshop name required"))
+    }
+    if (!description) {
+      reject(new Error("Workshop description required"))
+    }
+    inMemoryWorkshop.push({
+      name,
+      description
     })
+    resolve()
+  })
 }
 
 // function removeWorkshopByName(name) {
@@ -49,33 +49,33 @@ function addWorkshop(name, description) {
 // }
 
 function removeWorkshopById(id) {
-    return new Promise((resolve, reject) => {
-      if(id > inMemoryWorkshop.length || id < 0){
-        reject(new Error("Not valid id"))
-      }
-      inMemoryWorkshop.splice(id, 1)
-      resolve()
-    })
+  return new Promise((resolve, reject) => {
+    if(id > inMemoryWorkshop.length || id < 0){
+      reject(new Error("Not valid id"))
+    }
+    inMemoryWorkshop.splice(id, 1)
+    resolve()
+  })
 }
 
 
 function updateWorkshop(id, name, description) {
-   return new Promise((resolve, reject) => {
-        if (!name) {
-            reject(new Error("Workshop name required"))
-        }
-        if (!description) {
-            reject(new Error("Workshop description required"))
-        }
-        inMemoryWorkshop[id] = {name, description}
-        resolve()
-    })
+  return new Promise((resolve, reject) => {
+    if (!name) {
+      reject(new Error("Workshop name required"))
+    }
+    if (!description) {
+      reject(new Error("Workshop description required"))
+    }
+    inMemoryWorkshop[id] = {name, description}
+    resolve()
+  })
 }
 
 module.exports = {
-    getWorkshopList,
-    getWorkshopById,
-    addWorkshop,
-    removeWorkshopById,
-    updateWorkshop
+  getWorkshopList,
+  getWorkshopById,
+  addWorkshop,
+  removeWorkshopById,
+  updateWorkshop
 }
